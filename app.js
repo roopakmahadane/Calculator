cbeab89cb75e893195fd13baec43a34bab1fc13d
Roopak Mahadane <m.roopak@novasemita.org>
2019-10-18 11:54:03 +0530

    functionality achieved
    Fully functional basic calculator made. Styling remaning

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const button = document.querySelectorAll('.button');
 const textArea = document.querySelector('.text');
 const clear = document.querySelector('.clear');
 const equal= document.querySelector('.equal');
+const backspace = document.querySelector('.backspace');
+const percentage = document.querySelector('.percentage');
 
 function insert(e){
-  const num = e.target.value;
-  if(textArea.value == 0){
+  var num = e.currentTarget.value;
+  if(num == 'x'){
+    num = '*';
+  }
+  if(textArea.value == 0 && num > 0 && textArea.value !== '0.'){
     textArea.value = num;
   }else{
  textArea.value = textArea.value + num;
@@ -23,7 +28,27 @@ function equalTo(){
     textArea.value = eval(val);
   }
 }
+function backSpace(){
+  var val = textArea.value;
+  textArea.value = textArea.value.substring(0, val.length-1);
+}
+
+function percentCalculate(){
+    var val = textArea.value;
+    if(val){
+       textArea.value = eval(val)/100;
+    }
+
+}
 
 button.forEach((btn) => btn.addEventListener('click', insert));
 clear.addEventListener('click', clearAll);
 equal.addEventListener('click', equalTo);
+backspace.addEventListener('click', backSpace);
+percentage.addEventListener('click', percentCalculate);
+
+
+
+
+
+
