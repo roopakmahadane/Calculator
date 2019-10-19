e6241d29f8a475866406de5c3a14d02235fb444d
Roopak Mahadane <m.roopak@novasemita.org>
2019-10-19 16:49:07 +0530

    refactored, made it responsive

--- app.js
+++ app.js
@@ -4,30 +4,59 @@ const clear = document.querySelector('.clear');
 const equal= document.querySelector('.equal');
 const backspace = document.querySelector('.backspace');
 const percentage = document.querySelector('.percentage');
+const ans = document.querySelector(".answer");
 
+//Inserts value inside input field
 const insert = (e) => {
   var num = e.currentTarget.value;
-  if(num == 'x'){
-    num = '*';
-  }
- textArea.value == 0 && num > 0 && textArea.value !== '0.' ? textArea.value = num : textArea.value = textArea.value + num;
+ textArea.value == 0 && num > 0 && !textArea.value.includes('0.') ? textArea.value = num : textArea.value = textArea.value + num;
+ if(textArea.value.length*60 > window.screen.width){
+    textArea.style.fontSize = '5vh';
+ }
+  if(textArea.value.length*30 > window.screen.width){
+    textArea.style.fontSize = '3vh';
+ }
+
 };
 
+//displays answer
+const answerField = () => {
+  try{
+    if(textArea.value == '0' || textArea.value == ''){
+      // if text are is empty display 0
+         ans.innerHTML="";
+         textArea.value = '0';
+    }else{
+         var answer = eval(textArea.value.replace(/x/g, '*'));
+         ans.innerHTML = answer;
+    }
+  }
+ catch(e){
+    return
+ }
+}
+
+
 const clearAll = () => {
+  // To clear text area and display 0
   textArea.value = 0;
 }
 
+// when equal sign is clicked
 const equalTo = () => {
   var val = textArea.value;
   if(val){
-    textArea.value = eval(val);
+    textArea.value = eval(val.replace(/x/g, '*'));
   }
 }
+
+// to backspace one place
 const backSpace = () => {
   var val = textArea.value;
   textArea.value = textArea.value.substring(0, val.length-1);
 }
 
+//to calculate percentage
 const percentCalculate = () => {
     var val = textArea.value;
     if(val){
@@ -40,6 +69,9 @@ clear.addEventListener('click', clearAll);
 equal.addEventListener('click', equalTo);
 backspace.addEventListener('click', backSpace);
 percentage.addEventListener('click', percentCalculate);
+button.forEach((btn) => btn.addEventListener('click', answerField));
+
+
 
 
 
